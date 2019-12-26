d359ead4dcde5a50414a09fd67c7abbc0cce5f7e
Jason A. Donenfeld <Jason@zx2c4.com>
2019-12-26 18:10:37 +0100

    dns-hatchet: adjust path for new repo layout
    Reported-by: Joe Doss <joe@solidadmin.com>
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- contrib/dns-hatchet/apply.sh
+++ contrib/dns-hatchet/apply.sh
@@ -4,6 +4,6 @@
 # Copyright (C) 2015-2019 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.
 
 ME="$(readlink -f "$(dirname "$(readlink -f "$0")")")"
-TOOLS="$ME/../../../src/tools"
+TOOLS="$ME/../../src"
 
 sed -i "/~~ function override insertion point ~~/r $ME/hatchet.bash" "$TOOLS/wg-quick/linux.bash"

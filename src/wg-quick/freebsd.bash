96e42feb3f41e2161141d4958e2637d9dee6f90a
Jason A. Donenfeld <Jason@zx2c4.com>
2021-04-20 21:31:45 -0600

    wg-quick: kill route monitor when loop terminates
    If the route monitor doesn't attempt to write more to stdout, then this
    leaves a process hanging around. Kill it explicitly. We also switch to
    using exec in the process substitution, to reduce a bash process.
    
    Closes: https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=255286
    Reported-by: Christos Chatzaras <chris@cretaforce.gr>
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- src/wg-quick/freebsd.bash
+++ src/wg-quick/freebsd.bash
@@ -284,17 +284,19 @@ monitor_daemon() {
 	(make_temp
 	trap 'del_routes; clean_temp; exit 0' INT TERM EXIT
 	exec >/dev/null 2>&1
-	local event
+	exec 19< <(exec route -n monitor)
+	local event pid=$!
 	# TODO: this should also check to see if the endpoint actually changes
 	# in response to incoming packets, and then call set_endpoint_direct_route
 	# then too. That function should be able to gracefully cleanup if the
 	# endpoints change.
-	while read -r event; do
+	while read -u 19 -r event; do
 		[[ $event == RTM_* ]] || continue
 		ifconfig "$INTERFACE" >/dev/null 2>&1 || break
 		[[ $AUTO_ROUTE4 -eq 1 || $AUTO_ROUTE6 -eq 1 ]] && set_endpoint_direct_route
 		# TODO: set the mtu as well, but only if up
-	done < <(route -n monitor)) & disown
+	done
+	kill $pid) & disown
 }
 
 HAVE_SET_DNS=0

e6888dd74ee4215449517f43aba9c11a1633ea4e
Daniel Gröber <dxld@darkboxed.org>
2022-12-07 19:00:31 +0100

    wg-quick: run PreUp hook after creating interface
    Currently PreUp hooks run before the interface is created. This is
    problematic for moving the device into a Linux VRFs as this will
    currently clear all assigned IPv6 addressess (possibly a bug), so if we
    did this in PostUp (i.e. before add_addr) we'll have to manually re-add
    all assigned addresses. This is obviously less than ideal.
    
    Instead create the wg device just before running PreUp hooks. We apply
    this to all platforms for consistency.
    
    Test case:
    
        $ ip link add vrf-test type vrf table 1234
        $ ip link add wg-test type wireguard
        $ ip addr add dev wg-test 192.168.42.42/24
        $ ip addr add dev wg-test fe80::/64
    
        $ ip -br addr show wg-test
        wg-test          DOWN           192.168.42.42/24 fe80::/64
    
        $ ip link set dev wg-test master vrf-test
    
        $ ip -br addr show wg-test
        wg-test          DOWN           192.168.42.42/32
    
    Signed-off-by: Daniel Gröber <dxld@darkboxed.org>
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- src/wg-quick/darwin.bash
+++ src/wg-quick/darwin.bash
@@ -452,8 +452,8 @@ cmd_up() {
 	local i
 	get_real_interface && die "\`$INTERFACE' already exists as \`$REAL_INTERFACE'"
 	trap 'del_if; del_routes; exit' INT TERM EXIT
-	execute_hooks "${PRE_UP[@]}"
 	add_if
+	execute_hooks "${PRE_UP[@]}"
 	set_config
 	for i in "${ADDRESSES[@]}"; do
 		add_addr "$i"
